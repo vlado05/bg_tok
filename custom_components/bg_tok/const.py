c37fb914edf5c412e0d0c2d5ee3d3b82c1b6ca9c
vlado05 <v.yumerski@mail.ru>
2025-01-01 20:41:11 +0200

    Update const.py

--- custom_components/bg_tok/const.py
+++ custom_components/bg_tok/const.py
@@ -20,21 +20,24 @@ VAT_RATE = 0.2
 
 PROVIDER_PRICES = {
     # Section 6.1, https://www.dker.bg/uploads/reshenia/2024/res-c-17-2024.pdf
+    # https://www.dker.bg/uploads/reshenia/2025/res_c-03_25.pdf
     "electrohold": {
-        "дневна": .16210,
-        "нощна": .07104,
+        "дневна": .17564,
+        "нощна": .07698,
         "fees": .01354 + .00770 + .04203
     },
     # Section 6.1, https://www.dker.bg/uploads/reshenia/2024/res-c-17-2024.pdf
+    # https://www.dker.bg/uploads/reshenia/2025/res_c-03_25.pdf
     "evn": {
-        "дневна": .15926,
-        "нощна": .06833,
+        "дневна": .17257,
+        "нощна": .07404,
         "fees": .01354 + .00819 + .04106
     },
     # Section 6.3, https://www.dker.bg/uploads/reshenia/2024/res-c-17-2024.pdf
+    # https://www.dker.bg/uploads/reshenia/2025/res_c-03_25.pdf
     "energo_pro": {
-        "дневна": .16341,
-        "нощна": .06636,
-        "fees": .01354 + .00977 + .03689
+        "дневна": .17706,
+        "нощна": .07192,
+        "fees": .01354 + .00977 + .04135
     }
 }
